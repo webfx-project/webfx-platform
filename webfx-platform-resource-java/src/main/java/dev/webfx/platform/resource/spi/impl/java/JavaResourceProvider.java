4f0fd1282c2e74b6b6452aee506d7685b0824d9d
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-23 14:23:10 +0100

    Improved exception handling in JavaResourceProvider.toUrl()

--- webfx-platform-resource-java/src/main/java/dev/webfx/platform/resource/spi/impl/java/JavaResourceProvider.java
+++ webfx-platform-resource-java/src/main/java/dev/webfx/platform/resource/spi/impl/java/JavaResourceProvider.java
@@ -3,6 +3,7 @@ package dev.webfx.platform.resource.spi.impl.java;
 import dev.webfx.platform.resource.spi.ResourceProvider;
 
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Scanner;
 
 /**
@@ -12,7 +13,12 @@ public final class JavaResourceProvider implements ResourceProvider {
 
     @Override
     public String toUrl(String resourcePath, Class<?> loadingClass) {
-        return loadingClass.getResource(resourcePath).toExternalForm();
+        if (resourcePath == null)
+            return null;
+        URL resource = loadingClass.getResource(resourcePath);
+        if (resource == null)
+            throw new IllegalArgumentException("Resource not found: " + resourcePath);
+        return resource.toExternalForm();
     }
 
     @Override
