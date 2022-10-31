edecb06b1576d380cae7d7074aa9c5f7c297ec1f
Bruno Salmon <dev.salmonb@gmail.com>
2022-10-31 08:44:47 +0000

    Added missing file in previous commit

---
+++ webfx-platform-file-java/src/main/java/dev/webfx/platform/file/spi/impl/java/JavaPathBasedBlob.java
@@ -0,0 +1,26 @@
+package dev.webfx.platform.file.spi.impl.java;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * @author Bruno Salmon
+ */
+public abstract class JavaPathBasedBlob extends JavaBlob {
+
+    public JavaPathBasedBlob(Object platformBlob) {
+        super(platformBlob);
+    }
+
+    protected abstract Path toPath();
+
+    public String getMimeType() {
+        try {
+            return Files.probeContentType(toPath());
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+}
