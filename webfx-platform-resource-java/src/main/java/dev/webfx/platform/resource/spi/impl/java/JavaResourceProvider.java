83f038d5d54aaccddde9da15f9fa0bd794152f96
Bruno Salmon <dev.salmonb@gmail.com>
2022-12-02 17:54:45 +0000

    Fixed JavaResourceProvider: was not working in dev environment (when url = "file:...")

--- webfx-platform-resource-java/src/main/java/dev/webfx/platform/resource/spi/impl/java/JavaResourceProvider.java
+++ webfx-platform-resource-java/src/main/java/dev/webfx/platform/resource/spi/impl/java/JavaResourceProvider.java
@@ -2,8 +2,11 @@ package dev.webfx.platform.resource.spi.impl.java;
 
 import dev.webfx.platform.resource.spi.ResourceProvider;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 /**
@@ -23,12 +26,21 @@ public final class JavaResourceProvider implements ResourceProvider {
 
     @Override
     public String getText(String resourcePath) {
-        try (Scanner scanner = createScanner(getClass().getClassLoader().getResourceAsStream(resourcePath))) {
+        try (Scanner scanner = createScanner(getResourceInputStream(resourcePath))) {
             return scanner == null ? null : scanner.useDelimiter("\\A").next();
         }
     }
 
+    private InputStream getResourceInputStream(String resourcePath) {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resourcePath);
+        if (inputStream == null && resourcePath.startsWith("file:"))
+            try {
+                inputStream = new FileInputStream(resourcePath.substring(5));
+            } catch (FileNotFoundException e) { }
+        return inputStream;
+    }
+
     private static Scanner createScanner(InputStream inputStream) {
-        return inputStream == null ? null : new Scanner(inputStream, "UTF-8");
+        return inputStream == null ? null : new Scanner(inputStream, StandardCharsets.UTF_8);
     }
 }
