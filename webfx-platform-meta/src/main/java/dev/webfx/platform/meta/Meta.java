b9a1cb1c6b94a15a50cf3365fae98ae4a2354b61
Bruno Salmon <dev.salmonb@gmail.com>
2023-01-05 15:09:51 +0000

    Added webfx-platform-meta module

---
+++ webfx-platform-meta/src/main/java/dev/webfx/platform/meta/Meta.java
@@ -0,0 +1,49 @@
+package dev.webfx.platform.meta;
+
+import dev.webfx.platform.console.Console;
+import dev.webfx.platform.resource.Resource;
+
+import java.io.ByteArrayInputStream;
+import java.util.Properties;
+
+/**
+ * @author Bruno Salmon
+ */
+public final class Meta {
+
+    public final static String META_EXE_PACKAGE = "dev.webfx.platform.meta.exe";
+    public final static String META_EXE_RESOURCE_FOLDER = META_EXE_PACKAGE.replace('.', '/');
+    public final static String META_EXE_RESOURCE_FILE_NAME = "exe.properties";
+    public final static String META_EXE_RESOURCE_FILE_PATH = META_EXE_RESOURCE_FOLDER + "/" + META_EXE_RESOURCE_FILE_NAME;
+    public final static String META_EXE_EXECUTABLE_MODULE_NAME_KEY = "executableModuleName";
+    public final static String META_EXE_EXECUTABLE_MODULE_VERSION_KEY = "executableModuleVersion";
+    public final static String META_EXE_APPLICATION_MODULE_NAME_KEY = "applicationModuleName";
+
+    private static Properties META_PROPERTIES;
+
+    private static Properties getMetaProperties() {
+        if (META_PROPERTIES == null) {
+            META_PROPERTIES = new Properties();
+            try {
+                String content = Resource.getText(META_EXE_RESOURCE_FILE_PATH);
+                META_PROPERTIES.load(new ByteArrayInputStream(content.getBytes()));
+            } catch (Exception e) {
+                Console.log("Failed to read meta resource file " + META_EXE_RESOURCE_FILE_PATH, e);
+            }
+        }
+        return META_PROPERTIES;
+    }
+
+    public static String getExecutableModuleName() {
+        return getMetaProperties().getProperty(META_EXE_EXECUTABLE_MODULE_NAME_KEY);
+    }
+
+    public static String getExecutableModuleVersion() {
+        return getMetaProperties().getProperty(META_EXE_EXECUTABLE_MODULE_VERSION_KEY);
+    }
+
+    public static String getApplicationModuleName() {
+        return getMetaProperties().getProperty(META_EXE_APPLICATION_MODULE_NAME_KEY);
+    }
+
+}
---
+++ webfx-platform-meta/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.platform.meta {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires webfx.platform.console;
+    requires webfx.platform.resource;
+
+    // Exported packages
+    exports dev.webfx.platform.meta;
+
+}
