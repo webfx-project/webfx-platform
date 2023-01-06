ebe5f8dff1f2b8390d08062728a4732bfa7b0c2b
Bruno Salmon <dev.salmonb@gmail.com>
2023-01-06 12:27:48 +0000

    Minor change in GluonStorageLocationProvider exception message

--- webfx-platform-storagelocation-gluon/src/main/java/dev/webfx/platform/storagelocation/spi/impl/gluon/GluonStorageLocationProvider.java
+++ webfx-platform-storagelocation-gluon/src/main/java/dev/webfx/platform/storagelocation/spi/impl/gluon/GluonStorageLocationProvider.java
@@ -1,22 +1,21 @@
 package dev.webfx.platform.storagelocation.spi.impl.gluon;
 
 import com.gluonhq.attach.storage.StorageService;
-import com.gluonhq.attach.util.Services;
 import dev.webfx.platform.storagelocation.spi.StorageLocationProvider;
 
 /**
  * @author Bruno Salmon
  */
-public class GluonStorageLocationProvider implements StorageLocationProvider {
+public final class GluonStorageLocationProvider implements StorageLocationProvider {
 
     public String appInternalStoragePath;
 
     @Override
     public String getInternalStorageLocation() {
         if (appInternalStoragePath == null)
-            appInternalStoragePath = Services.get(StorageService.class)
+            appInternalStoragePath = StorageService.create()
                     .flatMap(StorageService::getPrivateStorage)
-                    .orElseThrow(() -> new IllegalStateException("Could not access private storage."))
+                    .orElseThrow(() -> new IllegalStateException("Could not get Gluon private storage."))
                     .getAbsolutePath();
         return appInternalStoragePath;
     }
