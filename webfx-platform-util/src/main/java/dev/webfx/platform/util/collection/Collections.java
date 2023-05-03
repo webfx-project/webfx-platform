b899819f90e9637f99f293b26a3f4b8139233cd3
Bruno Salmon <dev.salmonb@gmail.com>
2023-05-03 13:08:33 +0100

    Added a few mapOf() utility methods

--- webfx-platform-util/src/main/java/dev/webfx/platform/util/collection/Collections.java
+++ webfx-platform-util/src/main/java/dev/webfx/platform/util/collection/Collections.java
@@ -39,6 +39,30 @@ public final class Collections {
         return list;
     }
 
+    public static <K, V> Map<K, V> mapOf(K k1, V v1) {
+        Map<K, V> map = new HashMap<>();
+        map.put(k1, v1);
+        return map;
+    }
+
+    public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2) {
+        Map<K, V> map = mapOf(k1, v1);
+        map.put(k2, v2);
+        return map;
+    }
+
+    public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3) {
+        Map<K, V> map = mapOf(k1, v1, k2, v2);
+        map.put(k3, v3);
+        return map;
+    }
+
+    public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
+        Map<K, V> map = mapOf(k1, v1, k2, v2, k3, v3);
+        map.put(k4, v4);
+        return map;
+    }
+
     public static <T> void forEach(Iterable<T> iterable, Consumer<T> consumer) {
         // iterable.forEach(consumer); // Not GWT compilable for now
         if (iterable != null)
