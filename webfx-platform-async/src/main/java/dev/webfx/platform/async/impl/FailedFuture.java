28043aa2e3b86c032b4e9c68687cfa2c05cbba75
Bruno Salmon <dev.salmonb@gmail.com>
2023-10-19 14:07:53 +0100

    Added "Connection reset" stacktrace in FailedFuture for AWS issue

--- webfx-platform-async/src/main/java/dev/webfx/platform/async/impl/FailedFuture.java
+++ webfx-platform-async/src/main/java/dev/webfx/platform/async/impl/FailedFuture.java
@@ -42,6 +42,8 @@ public final class FailedFuture<T> extends FutureBase<T> {
     public FailedFuture(String failureMessage) {
         //this(null, failureMessage);
         this(new NoStackTraceThrowable(failureMessage));
+        if ("Connection reset".equals(failureMessage))
+            new Exception("Stacktrace for AWS").printStackTrace();
     }
 
     /**
