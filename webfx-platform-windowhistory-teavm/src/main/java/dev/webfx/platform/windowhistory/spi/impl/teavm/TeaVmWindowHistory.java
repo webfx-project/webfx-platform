de9a2881bd1dc4143c9c255b7bc649980ea38bcd
Bruno Salmon <dev.salmonb@gmail.com>
2025-05-10 16:33:22 +0100

    Added webfx-platform-windowhistory-teavm

---
+++ webfx-platform-windowhistory-teavm/src/main/java/dev/webfx/platform/windowhistory/spi/impl/teavm/TeaVmWindowHistory.java
@@ -0,0 +1,74 @@
+package dev.webfx.platform.windowhistory.spi.impl.teavm;
+
+import dev.webfx.platform.ast.ReadOnlyAstObject;
+import dev.webfx.platform.windowhistory.spi.impl.web.JsWindowHistory;
+import org.teavm.jso.JSBody;
+import org.teavm.jso.JSObject;
+import org.teavm.jso.browser.Window;
+import org.teavm.jso.dom.events.Event;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * @author Bruno Salmon
+ */
+public final class TeaVmWindowHistory implements JsWindowHistory {
+
+    public TeaVmWindowHistory() {}
+
+    private static org.teavm.jso.browser.History getCurrentHistory() {
+        return Window.current().getHistory();
+    }
+
+    @Override
+    public int length() {
+        return getCurrentHistory().getLength();
+    }
+
+    @Override
+    public void go(int offset) {
+        getCurrentHistory().go(offset);
+    }
+
+    @Override
+    public boolean supportsStates() {
+        return isFunction(getJSProperty(getCurrentHistory(), "pushState"));
+    }
+
+    @JSBody(params = { "obj", "prop" }, script = "return obj[prop];")
+    public static native JSObject getJSProperty(JSObject obj, String prop);
+
+    @JSBody(params = { "o" }, script = "return typeof o === 'function';")
+    public static native boolean isFunction(JSObject o);
+
+    @Override
+    public ReadOnlyAstObject state() {
+        return (ReadOnlyAstObject) getCurrentHistory().getState();
+    }
+
+    @Override
+    public void pushState(Object stateObj, String title, String url) {
+        getCurrentHistory().pushState((JSObject) stateObj, title, url);
+    }
+
+    @Override
+    public void replaceState(Object stateObj, String title, String url) {
+        getCurrentHistory().replaceState((JSObject) stateObj, title, url);
+    }
+
+    @Override
+    public void onPopState(Consumer<ReadOnlyAstObject> stateListener) {
+        Window.current().addEventListener("popstate", (Event evt) -> {
+            stateListener.accept((ReadOnlyAstObject) getJSProperty(evt, "state"));
+        });
+    }
+
+    @Override
+    public void onBeforeUnload(Function<ReadOnlyAstObject, String> listener) {
+        Window.current().addEventListener("onbeforeunload", (Event evt) -> {
+            listener.apply((ReadOnlyAstObject) evt);
+        });
+    }
+
+}
