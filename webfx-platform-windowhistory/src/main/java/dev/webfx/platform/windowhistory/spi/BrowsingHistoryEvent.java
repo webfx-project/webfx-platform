75a7a711118488bf60d3e00666076e44aa320f58
Bruno Salmon <dev.salmonb@gmail.com>
2025-05-17 19:15:57 +0100

    Fixed minor issues in window history comments

--- webfx-platform-windowhistory/src/main/java/dev/webfx/platform/windowhistory/spi/BrowsingHistoryEvent.java
+++ webfx-platform-windowhistory/src/main/java/dev/webfx/platform/windowhistory/spi/BrowsingHistoryEvent.java
@@ -1,10 +1,10 @@
 package dev.webfx.platform.windowhistory.spi;
 
 /**
- * An history event describes the type of change that has been done to a URL. Possible values are:
+ * A history event describes the type of change that has been done to a URL. Possible values are:
  * PUSHED – indicates a new item was added to the history
  * REPLACED – indicates the current item in history was altered
- * POPPED – indicates there is a new current item, i.e. the "current pointer" changed
+ * POPPED – indicates there is a new current item, i.e., the "current pointer" changed
  *
  * @author Bruno Salmon
  */
