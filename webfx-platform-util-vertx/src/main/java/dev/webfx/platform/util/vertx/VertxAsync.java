232fe606d55d66e91def5af0ea2ca4620da50abb
Bruno Salmon <dev.salmonb@gmail.com>
2025-10-08 13:38:52 +0100

    Reindented VertxAsync

--- webfx-platform-util-vertx/src/main/java/dev/webfx/platform/util/vertx/VertxAsync.java
+++ webfx-platform-util-vertx/src/main/java/dev/webfx/platform/util/vertx/VertxAsync.java
@@ -11,16 +11,16 @@ public final class VertxAsync {
     public static <T> Future<T> toWebfxFuture(io.vertx.core.Future<T> vertxFuture) {
         Promise<T> promise = Promise.promise();
         vertxFuture
-                .onFailure(promise::fail)
-                .onSuccess(promise::complete);
+            .onFailure(promise::fail)
+            .onSuccess(promise::complete);
         return promise.future();
     }
 
     public static <T> io.vertx.core.Future<T> toVertxFuture(Future<T> vertxFuture) {
         io.vertx.core.Promise<T> promise = io.vertx.core.Promise.promise();
         vertxFuture
-                .onFailure(promise::fail)
-                .onSuccess(promise::complete);
+            .onFailure(promise::fail)
+            .onSuccess(promise::complete);
         return promise.future();
     }
 
