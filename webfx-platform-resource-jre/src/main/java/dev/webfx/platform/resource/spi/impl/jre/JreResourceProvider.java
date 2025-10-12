16915b5494dba072734354e245d604f161bd39dd
Bruno Salmon <dev.salmonb@gmail.com>
2025-10-12 13:36:39 +0100

    Added TeaVMResourceProvider in new webfx-platform-resource-teavm module

--- webfx-platform-resource-jre/src/main/java/dev/webfx/platform/resource/spi/impl/jre/JreResourceProvider.java
+++ webfx-platform-resource-jre/src/main/java/dev/webfx/platform/resource/spi/impl/jre/JreResourceProvider.java
@@ -2,10 +2,12 @@ package dev.webfx.platform.resource.spi.impl.jre;
 
 import dev.webfx.platform.resource.spi.ResourceProvider;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
 /**
  * @author Bruno Salmon
@@ -38,8 +40,28 @@ public class JreResourceProvider implements ResourceProvider {
 
     @Override
     public String getText(String resourcePath) {
+/* Modern code but doesn't compile with TeaVM (TeaVMResourceProvider extends this class), so we use the old-style code.
         try (Scanner scanner = createScanner(getResourceInputStream(resourcePath))) {
             return scanner == null ? null : scanner.useDelimiter("\\A").next();
+        }
+*/
+        try (InputStream is = getResourceInputStream(resourcePath)) {
+            if (is == null) {
+                return null;
+            }
+            StringBuilder sb = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (!sb.isEmpty()) {
+                        sb.append('\n');
+                    }
+                    sb.append(line);
+                }
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            return null;
         }
     }
 
@@ -48,19 +70,20 @@ public class JreResourceProvider implements ResourceProvider {
         try {
             // trying directly with the provided resourcePath
             // Note: this may fail if resourcePath is actually a URL path such as file:/... => raises an exception on
-            // Windows, but just return null on other platforms
+            // Windows, but just returns null on other platforms
             inputStream = getClass().getClassLoader().getResourceAsStream(resourcePath);
-        } catch (Exception e) { }
+        } catch (Exception ignored) { }
         if (inputStream == null) {
             try {
                 // Now trying with a URL path
                 inputStream = new URL(resourcePath).openStream();
-            } catch (Exception e) { }
+            } catch (Exception ignored) { }
         }
         return inputStream;
     }
 
+    /* Not used anymore due to TeaVM incompatibility
     private static Scanner createScanner(InputStream inputStream) {
         return inputStream == null ? null : new Scanner(inputStream, StandardCharsets.UTF_8);
-    }
+    }*/
 }
---
+++ webfx-platform-resource-teavm/src/main/java/dev/webfx/platform/resource/spi/impl/teavm/TeaVMResourceProvider.java
@@ -0,0 +1,10 @@
+package dev.webfx.platform.resource.spi.impl.teavm;
+
+import dev.webfx.platform.resource.spi.impl.jre.JreResourceProvider;
+
+/**
+ * @author Bruno Salmon
+ */
+public final class TeaVMResourceProvider extends JreResourceProvider {
+
+}
