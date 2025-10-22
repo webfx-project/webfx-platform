0643ad2223f2955db6565c41502ad8358777a5f6
Bruno Salmon <dev.salmonb@gmail.com>
2025-10-22 17:59:01 +0100

    Fixed non working JsArray.of() in elemental2 polyfill for TeaVM

--- webfx-platform-teavm-elemental2-polyfill/src/main/java/elemental2/core/JsArray.java
+++ webfx-platform-teavm-elemental2-polyfill/src/main/java/elemental2/core/JsArray.java
@@ -1,8 +1,11 @@
 package elemental2.core;
 
+import dev.webfx.platform.util.teavm.TeaVmUtil;
 import elemental2.dom.JsArrayLike;
 import elemental2.dom.JsIterable;
 import org.teavm.jso.JSClass;
+import org.teavm.jso.JSObject;
+import org.teavm.jso.core.JSArray;
 
 /**
  * @author Bruno Salmon
@@ -10,6 +13,18 @@ import org.teavm.jso.JSClass;
 @JSClass(name = "Array")
 public class JsArray<T> implements JsIterable<T>, ReadonlyArray<T>, JsArrayLike<T> {
 
-    public static native <T> JsArray<T> of(T... var_args);
+    public static <T> JsArray<T> of(T... var_args) {
+        JSArray array = JSArray.create(var_args.length);
+        for (int i = 0; i < var_args.length; ++i) {
+            T arg = var_args[i];
+            if (arg instanceof Number n)
+                TeaVmUtil.setAt(array, i, n.doubleValue());
+            else if (arg instanceof Boolean b)
+                TeaVmUtil.setAt(array, i, b.booleanValue());
+            else
+                TeaVmUtil.setAt(array, i, arg);
+        }
+        return (JsArray<T>) (JSObject) array;
+    }
 
 }
